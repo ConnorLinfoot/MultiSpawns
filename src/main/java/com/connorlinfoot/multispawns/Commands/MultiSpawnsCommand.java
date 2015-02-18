3f17db3880272fdc9dbda3c4052635a74794fb12
Connor Linfoot <connorlinfoot@icloud.com>
2015-02-18 14:18:04 +0000

    I promise I'll use an IDE later

--- src/main/java/com/connorlinfoot/multispawns/Commands/MultiSpawnsCommand.java
+++ src/main/java/com/connorlinfoot/multispawns/Commands/MultiSpawnsCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MultiSpawnsCommand implements CommandExecutor {
 
